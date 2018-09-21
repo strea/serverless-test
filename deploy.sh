398ab5f43c907dd3cf73e44de247a37fc575f6fd
strea <henri.hemminki@gmail.com>
2018-09-21 22:31:58 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-
 echo "Installing serverless"
 echo "====================="
+npm install
 npm install -g serverless
-
 echo "Deploying app to $env"
 echo "====================="
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
