91f7fb097beb1d44a40c9a30fdd30afa5765753c
strea <henri.hemminki@gmail.com>
2018-09-21 22:18:13 +0300

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Installing serverless"
+echo "====================="
+npm install -g serverless
+
+echo "Deploying app to $env"
+echo "====================="
+serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
